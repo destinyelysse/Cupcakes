d8d562967e98492af7dd2f0d7d7f238e88b82ffa
Destiny Serna <destinyeserna@gmail.com>
2021-01-04 20:14:31 -0800

    Initial Commit

---
+++ Cupcakes/CupcakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakesApp.swift
+//  Cupcakes
+//
+//  Created by Destiny Serna on 1/4/21.
+//
+
+import SwiftUI
+
+@main
+struct CupcakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
